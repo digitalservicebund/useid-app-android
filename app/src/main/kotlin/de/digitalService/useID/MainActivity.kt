d3b38080eff99ed3cf4e1d71860a48261a704b1e
Fabio Tacke <fabio@tacke.berlin>
2022-05-06 08:03:32 +0200

    Add second onboarding screen

--- app/src/main/kotlin/de/digitalService/useID/MainActivity.kt
+++ app/src/main/kotlin/de/digitalService/useID/MainActivity.kt
@@ -3,6 +3,7 @@ package de.digitalService.useID
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
@@ -22,6 +23,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import de.digitalService.useID.ui.theme.UseIDTheme
 
@@ -50,65 +52,119 @@ fun UseIDApp() {
                             )
                         }
                     },
-                colors = TopAppBarDefaults.smallTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.background,
-                    navigationIconContentColor = MaterialTheme.colorScheme.onBackground
-                )
-                )
-            }
-        ){ paddingValues ->
-            Column(
-                modifier = Modifier
-                    .padding(top = paddingValues.calculateTopPadding())
-                    .fillMaxHeight()
-            ) {
-                Column(modifier = Modifier
-                    .weight(1f)
-                    .verticalScroll(rememberScrollState())
-                ) {
-                    Column(modifier = Modifier.padding(horizontal = 20.dp)) {
-                        Text(
-                            "Haben Sie Ihren Online-Ausweis bereits benutzt?",
-                            style = MaterialTheme.typography.titleLarge
-                        )
-                        Spacer(modifier = Modifier.height(20.dp))
-                        Text(
-                            "Folgende Dokumente bieten die Funktion an:\nDeutscher Personalausweis, Elektronischer Aufenthaltstitel, eID-Karte für Unionsbürger",
-                            style = MaterialTheme.typography.bodySmall,
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(20.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.eids),
-                        contentScale = ContentScale.FillWidth,
-                        contentDescription = ""
+                    colors = TopAppBarDefaults.smallTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.background,
+                        navigationIconContentColor = MaterialTheme.colorScheme.onBackground
                     )
-                }
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .padding(start = 20.dp, top = 20.dp, end = 20.dp, bottom = 20.dp)
-                ) {
-                    Button(
-                        onClick = { }, colors = ButtonDefaults.buttonColors(
-                            containerColor = MaterialTheme.colorScheme.secondary,
-                            contentColor = MaterialTheme.colorScheme.onSecondary
-                        ), shape = MaterialTheme.shapes.small, modifier = Modifier.fillMaxWidth().height(60.dp)
-                    ) {
-                        Text("Ja, ich habe es bereits genutzt")
-                    }
-                    Spacer(modifier = Modifier.height(15.dp))
-                    Button(
-                        onClick = { }, colors = ButtonDefaults.buttonColors(
-                            containerColor = MaterialTheme.colorScheme.primary,
-                            contentColor = MaterialTheme.colorScheme.onPrimary
-                        ), shape = MaterialTheme.shapes.small, modifier = Modifier.fillMaxWidth().height(60.dp)
-                    ) {
-                        Text("Nein, jetzt Online-Ausweis einrichten")
-                    }
-                }
+                )
             }
+        ) { paddingValues ->
+            FirstTimeUserPINLetterScreen(topPadding = paddingValues.calculateTopPadding())
         }
+    }
+}
+
+@Composable
+fun FirstTimeUserCheckScreen(topPadding: Dp) {
+    OnboardingScreen(
+        title = "Haben Sie Ihren Online-Ausweis bereits benutzt?",
+        body = "Folgende Dokumente bieten die Funktion an:\nDeutscher Personalausweis, Elektronischer Aufenthaltstitel, eID-Karte für Unionsbürger",
+        imageID = R.drawable.eids,
+        imageScaling = ContentScale.Inside,
+        topPadding = topPadding,
+        primaryButtonAction = { },
+        primaryButtonLabel = "Ja, ich habe es bereits genutzt",
+        secondaryButtonAction = { },
+        secondaryButtonLabel = "Nein, jetzt Online-Ausweis einrichten"
+    )
+}
+
+@Composable
+fun FirstTimeUserPINLetterScreen(topPadding: Dp) {
+    OnboardingScreen(
+        title = "Haben Sie noch Ihren PIN-Brief?",
+        body = "Der PIN-Brief wurde Ihnen nach der Beantragung des Ausweises zugesandt.",
+        imageID = R.drawable.pin_brief,
+        imageScaling = ContentScale.FillWidth,
+        topPadding = topPadding,
+        primaryButtonAction = { },
+        primaryButtonLabel = "Ja, PIN-Brief vorhanden",
+        secondaryButtonAction = { },
+        secondaryButtonLabel = "Nein, neuen PIN-Brief bestellen"
+    )
+}
+
+@Composable
+fun OnboardingScreen(title: String, body: String, @DrawableRes imageID: Int, imageScaling: ContentScale, primaryButtonLabel: String, primaryButtonAction: () -> Unit, secondaryButtonLabel: String, secondaryButtonAction: () -> Unit, topPadding: Dp) {
+    Column(
+        modifier = Modifier
+            .padding(top = topPadding)
+            .fillMaxHeight()
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+                Text(
+                    title,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    body,
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Image(
+                painter = painterResource(id = imageID),
+                contentScale = imageScaling,
+                contentDescription = ""
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(start = 20.dp, top = 20.dp, end = 20.dp, bottom = 20.dp)
+        ) {
+            BundButton(type = ButtonType.PRIMARY, onClick = primaryButtonAction, label = primaryButtonLabel)
+            Spacer(modifier = Modifier.height(15.dp))
+            BundButton(type = ButtonType.SECONDARY, onClick = secondaryButtonAction, label = secondaryButtonLabel)
+        }
+    }
+}
+
+enum class ButtonType {
+    PRIMARY, SECONDARY
+}
+
+@Composable
+fun BundButton(type: ButtonType, onClick: () -> Unit, label: String) {
+    val containerColor: Color
+    val contentColor: Color
+
+    when (type) {
+        ButtonType.PRIMARY -> {
+            containerColor = MaterialTheme.colorScheme.primary
+            contentColor = MaterialTheme.colorScheme.onPrimary
+        }
+        ButtonType.SECONDARY -> {
+            containerColor = MaterialTheme.colorScheme.secondary
+            contentColor = MaterialTheme.colorScheme.onSecondary
+        }
+    }
+
+    Button(
+        onClick = onClick, colors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+            contentColor = contentColor
+        ), shape = MaterialTheme.shapes.small, modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Text(label)
     }
 }
 
