6bf256cdef2b0177af22e9888d78a8312ee2f1b5
Ronny Brzeski <SvenBrzeski3@freenet.de>
2022-06-15 11:44:40 +0200

    Add SetupTransportPINViewModelTest

---
+++ app/src/test/kotlin/de/digitalService/useID/SetupTransportPINViewModelTest.kt
@@ -0,0 +1,108 @@
+package de.digitalService.useID
+
+import de.digitalService.useID.ui.composables.screens.SetupTransportPINViewModel
+import de.digitalService.useID.ui.coordinators.SetupCoordinator
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class SetupTransportPINViewModelTest {
+
+    @MockK(relaxed = true)
+    lateinit var coordinatorMock: SetupCoordinator
+
+    @MockK(relaxed = true)
+    lateinit var secureStorageManagerMock: SecureStorageManager
+
+    private val defaultPin = ""
+
+    @Test
+    fun onInputChanged() {
+        val testValue = "12345"
+
+        val viewModel = SetupTransportPINViewModel(
+            coordinatorMock,
+            secureStorageManagerMock
+        )
+
+        viewModel.onInputChanged(testValue)
+
+        Assertions.assertEquals(testValue, viewModel.transportPIN)
+
+        verify(exactly = 0) { coordinatorMock.onTransportPINEntered() }
+        verify(exactly = 0) { secureStorageManagerMock.setTransportPIN(testValue) }
+    }
+
+    @Test
+    fun onDoneTapped_Success() {
+        val testValue = "12345"
+
+        val viewModel = SetupTransportPINViewModel(
+            coordinatorMock,
+            secureStorageManagerMock
+        )
+
+        viewModel.onInputChanged(testValue)
+        viewModel.onDoneTapped()
+
+        Assertions.assertEquals(defaultPin, viewModel.transportPIN)
+
+        verify(exactly = 1) { coordinatorMock.onTransportPINEntered() }
+        verify(exactly = 1) { secureStorageManagerMock.setTransportPIN(testValue) }
+    }
+
+    @Test
+    fun onDoneTapped_NoPreviousInput() {
+        val viewModel = SetupTransportPINViewModel(
+            coordinatorMock,
+            secureStorageManagerMock
+        )
+
+        viewModel.onDoneTapped()
+
+        Assertions.assertEquals(defaultPin, viewModel.transportPIN)
+
+        verify(exactly = 0) { coordinatorMock.onTransportPINEntered() }
+        verify(exactly = 0) { secureStorageManagerMock.setTransportPIN(any()) }
+    }
+
+    @Test
+    fun onDoneTapped_TooShort() {
+        val testValue = "1234"
+
+        val viewModel = SetupTransportPINViewModel(
+            coordinatorMock,
+            secureStorageManagerMock
+        )
+
+        viewModel.onInputChanged(testValue)
+        viewModel.onDoneTapped()
+
+        Assertions.assertEquals(testValue, viewModel.transportPIN)
+
+        verify(exactly = 0) { coordinatorMock.onTransportPINEntered() }
+        verify(exactly = 0) { secureStorageManagerMock.setTransportPIN(testValue) }
+    }
+
+    @Test
+    fun onDoneTapped_TooLong() {
+        val testValue = "123456"
+
+        val viewModel = SetupTransportPINViewModel(
+            coordinatorMock,
+            secureStorageManagerMock
+        )
+
+        viewModel.onInputChanged(testValue)
+        viewModel.onDoneTapped()
+
+        Assertions.assertEquals(testValue, viewModel.transportPIN)
+
+        verify(exactly = 0) { coordinatorMock.onTransportPINEntered() }
+        verify(exactly = 0) { secureStorageManagerMock.setTransportPIN(testValue) }
+    }
+}
