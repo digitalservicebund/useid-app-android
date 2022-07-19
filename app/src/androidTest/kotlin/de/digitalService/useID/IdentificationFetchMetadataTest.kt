361d67f4467302822eb126da258f1a0027df01b6
Ronny Brzeski <SvenBrzeski3@freenet.de>
2022-07-19 15:55:51 +0200

    Add IdentificationFetchMetadataViewModelTest

---
+++ app/src/androidTest/kotlin/de/digitalService/useID/IdentificationFetchMetadataTest.kt
@@ -0,0 +1,76 @@
+package de.digitalService.useID
+
+import androidx.activity.compose.setContent
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import de.digitalService.useID.ui.composables.screens.identification.IdentificationFetchMetadata
+import de.digitalService.useID.ui.composables.screens.identification.IdentificationFetchMetadataViewModelInterface
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class IdentificationFetchMetadataTest {
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @get:Rule(order = 0)
+    var hiltRule = HiltAndroidRule(this)
+
+    @Test
+    fun errorNotShown() {
+        val viewModel: IdentificationFetchMetadataViewModelInterface = mockk(relaxUnitFun = true)
+
+        every { viewModel.shouldShowProgressIndicator } returns true
+        every { viewModel.shouldShowError } returns false
+
+        composeTestRule.activity.setContent {
+            IdentificationFetchMetadata(viewModel = viewModel)
+        }
+
+        composeTestRule.waitForIdle()
+
+        verify(exactly = 1) { viewModel.fetchMetadata() }
+
+        val errorDialogTitle = composeTestRule.activity.getString(R.string.identification_fetchMetadataError_title)
+        composeTestRule.onNodeWithText(errorDialogTitle).assertDoesNotExist()
+    }
+
+    @Test
+    fun error() {
+        val viewModel: IdentificationFetchMetadataViewModelInterface = mockk(relaxUnitFun = true)
+
+        every { viewModel.shouldShowProgressIndicator } returns true
+        every { viewModel.shouldShowError } returns true
+
+        composeTestRule.activity.setContent {
+            IdentificationFetchMetadata(viewModel = viewModel)
+        }
+
+        composeTestRule.waitForIdle()
+
+        verify(exactly = 1) { viewModel.fetchMetadata() }
+
+        val errorDialogTitle = composeTestRule.activity.getString(R.string.identification_fetchMetadataError_title)
+        composeTestRule.onNodeWithText(errorDialogTitle).assertIsDisplayed()
+
+        val errorRetryButtonTitle = composeTestRule.activity.getString(R.string.identification_fetchMetadataError_button)
+        composeTestRule.onNodeWithText(errorRetryButtonTitle).performClick()
+
+        verify { viewModel.onErrorRetry() }
+
+        val errorCancelButtonTitle = composeTestRule.activity.getString(R.string.navigation_cancel)
+        composeTestRule.onNodeWithContentDescription(errorCancelButtonTitle).performClick()
+
+        verify { viewModel.onErrorCancel() }
+
+    }
+}
---
+++ app/src/test/kotlin/de/digitalService/useID/IdentificationFetchMetadataViewModelTest.kt
@@ -0,0 +1,101 @@
+package de.digitalService.useID
+
+import de.digitalService.useID.ui.composables.screens.identification.FetchMetadataEvent
+import de.digitalService.useID.ui.composables.screens.identification.IdentificationFetchMetadataViewModel
+import de.digitalService.useID.ui.coordinators.IdentificationCoordinator
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class IdentificationFetchMetadataViewModelTest {
+
+
+    @MockK(relaxUnitFun = true)
+    lateinit var mockIdentificationCoordinator: IdentificationCoordinator
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val dispatcher = StandardTestDispatcher()
+
+    @BeforeEach
+    fun setup() {
+        Dispatchers.setMain(dispatcher)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun init_collectMetaDataEvents_started() {
+        every { mockIdentificationCoordinator.fetchMetadataEventFlow } returns
+            MutableStateFlow(FetchMetadataEvent.Started)
+
+        val viewModel = IdentificationFetchMetadataViewModel(
+            mockIdentificationCoordinator,
+        )
+        
+        dispatcher.scheduler.advanceUntilIdle();
+
+        assertFalse(viewModel.shouldShowError)
+        assertTrue(viewModel.shouldShowProgressIndicator)
+    }
+
+    @Test
+    fun init_collectMetaDataEvents_error() {
+        every { mockIdentificationCoordinator.fetchMetadataEventFlow } returns
+            MutableStateFlow(FetchMetadataEvent.Error)
+
+        val viewModel = IdentificationFetchMetadataViewModel(
+            mockIdentificationCoordinator,
+        )
+
+        dispatcher.scheduler.advanceUntilIdle();
+
+        assertTrue(viewModel.shouldShowError)
+        assertFalse(viewModel.shouldShowProgressIndicator)
+    }
+
+    @Test
+    fun init_collectMetaDataEvents_finished() {
+        every { mockIdentificationCoordinator.fetchMetadataEventFlow } returns
+            MutableStateFlow(FetchMetadataEvent.Finished)
+
+        val viewModel = IdentificationFetchMetadataViewModel(
+            mockIdentificationCoordinator,
+        )
+
+        dispatcher.scheduler.advanceUntilIdle();
+
+        assertFalse(viewModel.shouldShowError)
+        assertFalse(viewModel.shouldShowProgressIndicator)
+    }
+
+    @Test
+    fun fetchMetaDataEvents() {
+        every { mockIdentificationCoordinator.fetchMetadataEventFlow } returns
+            MutableStateFlow(FetchMetadataEvent.Started)
+
+        val viewModel = IdentificationFetchMetadataViewModel(
+            mockIdentificationCoordinator,
+        )
+
+        viewModel.fetchMetadata()
+
+        verify(exactly = 1) { mockIdentificationCoordinator.startIdentificationProcess() }
+    }
+}
