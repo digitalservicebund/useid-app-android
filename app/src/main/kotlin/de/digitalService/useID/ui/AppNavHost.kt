cdd29bca69353a3eca996bc88cb3325516f91c3c
Ronny Brzeski <SvenBrzeski3@freenet.de>
2022-08-17 16:35:03 +0200

    Fix preview version

--- app/src/main/kotlin/de/digitalService/useID/ui/AppNavHost.kt
+++ app/src/main/kotlin/de/digitalService/useID/ui/AppNavHost.kt
@@ -5,10 +5,10 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.manualcomposablecalls.composable
-import de.digitalService.useID.ui.components.ConfigSpecificIdentificationFetchMetadata
-import de.digitalService.useID.ui.components.ConfigSpecificIdentificationScan
-import de.digitalService.useID.ui.components.ConfigSpecificSetupScan
-import de.digitalService.useID.ui.screens.*
+import de.digitalService.useID.ui.composables.ConfigSpecificIdentificationFetchMetadata
+import de.digitalService.useID.ui.composables.ConfigSpecificIdentificationScan
+import de.digitalService.useID.ui.composables.ConfigSpecificSetupScan
+import de.digitalService.useID.ui.screens.NavGraphs
 import de.digitalService.useID.ui.screens.destinations.IdentificationFetchMetadataDestination
 import de.digitalService.useID.ui.screens.destinations.IdentificationScanDestination
 import de.digitalService.useID.ui.screens.destinations.SetupScanDestination
