48c25db05ce1479079b21d2e8da3cf6fb928025f
Ronny Brzeski <SvenBrzeski3@freenet.de>
2022-08-18 14:49:10 +0200

    Make what is nfc body not bold

--- app/src/main/kotlin/de/digitalService/useID/ui/dialogs/WhatIsNfcDialog.kt
+++ app/src/main/kotlin/de/digitalService/useID/ui/dialogs/WhatIsNfcDialog.kt
@@ -1,18 +1,18 @@
 package de.digitalService.useID.ui.dialogs
 
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import de.digitalService.useID.R
-import de.digitalService.useID.models.ScanError
 import de.digitalService.useID.ui.theme.UseIDTheme
 
 @Composable
 fun WhatIsNfcDialog(onButtonTap: () -> Unit) {
     StandardDialog(
         title = { Text(stringResource(id = R.string.whatIsNfc_title), style = MaterialTheme.typography.titleMedium) },
-        text = { Text(stringResource(id = R.string.whatIsNfc_body), style = MaterialTheme.typography.bodyMedium) },
+        text = { Text(stringResource(id = R.string.whatIsNfc_body), style = MaterialTheme.typography.bodySmall) },
         onButtonTap = onButtonTap
     )
 }
@@ -21,6 +21,6 @@ fun WhatIsNfcDialog(onButtonTap: () -> Unit) {
 @Composable
 private fun Preview() {
     UseIDTheme {
-        ScanErrorAlertDialog(error = ScanError.PINBlocked, onButtonTap = { })
+        WhatIsNfcDialog(onButtonTap = { })
     }
 }
