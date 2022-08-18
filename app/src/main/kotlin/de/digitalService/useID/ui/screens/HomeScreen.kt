65a5969e760fe8d6e4403c68038dcc2b9860ee00
Ronny Brzeski <SvenBrzeski3@freenet.de>
2022-08-18 16:11:03 +0200

    Redesign HomeScreen

--- app/src/main/kotlin/de/digitalService/useID/ui/screens/HomeScreen.kt
+++ app/src/main/kotlin/de/digitalService/useID/ui/screens/HomeScreen.kt
@@ -2,12 +2,9 @@
 
 package de.digitalService.useID.ui.screens
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,13 +14,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import dagger.hilt.android.lifecycle.HiltViewModel
+import de.digitalService.useID.BuildConfig
 import de.digitalService.useID.R
 import de.digitalService.useID.ui.components.ButtonType
 import de.digitalService.useID.ui.components.RegularBundButton
@@ -33,10 +33,7 @@ import de.digitalService.useID.ui.screens.destinations.AccessibilityScreenDestin
 import de.digitalService.useID.ui.screens.destinations.ImprintScreenDestination
 import de.digitalService.useID.ui.screens.destinations.PrivacyScreenDestination
 import de.digitalService.useID.ui.screens.destinations.TermsOfUseScreenDestination
-import de.digitalService.useID.ui.theme.Black
-import de.digitalService.useID.ui.theme.Gray300
-import de.digitalService.useID.ui.theme.Gray600
-import de.digitalService.useID.ui.theme.UseIDTheme
+import de.digitalService.useID.ui.theme.*
 import javax.inject.Inject
 
 @Composable
@@ -47,35 +44,65 @@ fun HomeScreen(viewModel: HomeScreenViewModelInterface = hiltViewModel<HomeScree
         viewModel.homeScreenLaunched()
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(horizontal = 16.dp)
-            .verticalScroll(rememberScrollState())
-    ) {
-        Text(
-            text = stringResource(R.string.homeScreen_title),
-            style = MaterialTheme.typography.titleLarge
-        )
-        Text(text = "Platzhalter text")
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Blue300)
+        ) {
+            Image(painter = painterResource(id = R.drawable.pin_brief), contentDescription = "")
 
-        Spacer(modifier = Modifier.height(300.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Text(
-            text = stringResource(R.string.homeScreen_more_subtitle),
-            style = MaterialTheme.typography.titleLarge
-        )
+            Text(
+                text = "Ausweisen",
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(horizontal = 32.dp)
+            )
+            Text(
+                text = "Öffnen Sie auf Ihrem Smartphone die Internetseite vom Anbieter und drücken Sie auf den Button »Mit Bundes Login ausweisen«",
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(horizontal = 64.dp)
+            )
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(32.dp))
+        }
 
-        SetupUseIdCardBox(viewModel)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(horizontal = 16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = stringResource(R.string.homeScreen_more_subtitle),
+                style = MaterialTheme.typography.titleLarge
+            )
 
-        MoreSettingsCardBox(viewModel)
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+            SetupUseIdCardBox(viewModel)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            MoreSettingsCardBox(viewModel)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Version: ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})",
+                color = Gray900,
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
     }
 }
 
@@ -84,7 +111,7 @@ private fun SetupUseIdCardBox(viewModel: HomeScreenViewModelInterface) {
     Card(
         shape = RoundedCornerShape(8.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White),
-        border = BorderStroke(1.dp, Gray600),
+        border = BorderStroke(1.dp, Gray300),
         modifier = Modifier
             .fillMaxWidth()
     ) {
@@ -94,12 +121,12 @@ private fun SetupUseIdCardBox(viewModel: HomeScreenViewModelInterface) {
                 contentScale = ContentScale.Inside,
                 contentDescription = "",
                 modifier = Modifier
-                    .offset(y = -20.dp)
-                    .padding(bottom = 20.dp)
+                    .padding(bottom = 40.dp)
+                    .padding(horizontal = 8.dp)
             )
 
             RegularBundButton(
-                type = ButtonType.SECONDARY,
+                type = ButtonType.PRIMARY,
                 onClick = viewModel::setupOnlineID,
                 label = stringResource(R.string.homeScreen_setupOnlineID_button),
                 modifier = Modifier
@@ -117,17 +144,11 @@ private fun MoreSettingsCardBox(viewModel: HomeScreenViewModelInterface) {
     Card(
         shape = RoundedCornerShape(8.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White),
-        border = BorderStroke(1.dp, Gray600),
+        border = BorderStroke(1.dp, Gray300),
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        CardButton(text = stringResource(R.string.homeScreen_more_settings_button), onClick = {})
-        StyledDivider()
-
-        CardButton(text = stringResource(R.string.homeScreen_more_askedQuestions_button), onClick = {})
-        StyledDivider()
-
-        CardButton(text = stringResource(R.string.homeScreen_more_privacy_button), onClick = viewModel::onPrivacyButtonClicked)
+        CardButton(text = stringResource(R.string.homeScreen_more_license_button), onClick = {})
         StyledDivider()
 
         CardButton(
@@ -136,10 +157,10 @@ private fun MoreSettingsCardBox(viewModel: HomeScreenViewModelInterface) {
         )
         StyledDivider()
 
-        CardButton(text = stringResource(R.string.homeScreen_more_imprint_button), onClick = viewModel::onImprintButtonClicked)
+        CardButton(text = stringResource(R.string.homeScreen_more_termsOfUse_button), onClick = viewModel::onTermsOfUseButtonClicked)
         StyledDivider()
 
-        CardButton(text = stringResource(R.string.homeScreen_more_legalNotice_button), onClick = viewModel::onTermsOfUseButtonClicked)
+        CardButton(text = stringResource(R.string.homeScreen_more_imprint_button), onClick = viewModel::onImprintButtonClicked)
     }
 }
 
