572756c050c8abf0c67e43682b9f0802398c3e0d
Ronny Brzeski <SvenBrzeski3@freenet.de>
2022-09-20 15:47:20 +0200

    Add back button for LicenseScreen

--- app/src/main/kotlin/de/digitalService/useID/ui/screens/information/DependenciesScreen.kt
+++ app/src/main/kotlin/de/digitalService/useID/ui/screens/information/DependenciesScreen.kt
@@ -2,6 +2,7 @@ package de.digitalService.useID.ui.screens.information
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
@@ -15,39 +16,54 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.mikepenz.aboutlibraries.entity.License
 import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 import de.digitalService.useID.R
+import de.digitalService.useID.ui.components.NavigationButton
+import de.digitalService.useID.ui.components.NavigationIcon
+import de.digitalService.useID.ui.components.ScreenWithTopBar
 
 @Destination
 @Composable
-fun DependenciesScreen() {
-    var dialogText: Set<License>? by remember { mutableStateOf(null) }
+fun DependenciesScreen(
+    navigator: DestinationsNavigator
+) {
+    ScreenWithTopBar(
+        navigationButton = NavigationButton(icon = NavigationIcon.Back, onClick = { navigator.navigateUp() })
+    ) { topPadding ->
+        var dialogText: Set<License>? by remember { mutableStateOf(null) }
 
-    LibrariesContainer(Modifier.fillMaxSize()) {
-        dialogText = it.licenses
-    }
+        LibrariesContainer(
+            Modifier
+                .fillMaxSize()
+                .padding(top = topPadding)
+        ) {
+            dialogText = it.licenses
+        }
 
-    dialogText?.let {
-        AlertDialog(
-            onDismissRequest = { dialogText = null },
-            text = {
-                Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                    it.forEach {
-                        Text(text = "${it.name}, ${it.year}\n ${it.licenseContent}")
+        dialogText?.let {
+            AlertDialog(
+                onDismissRequest = { dialogText = null },
+                text = {
+                    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                        it.forEach {
+                            Text(text = "${it.name}, ${it.year}\n ${it.licenseContent}")
+                        }
                     }
-                }
-            },
-            confirmButton = {
-                Button(onClick = { dialogText = null }) {
-                    Text(text = stringResource(R.string.licenseDialog_confirm))
-                }
-            },
-            containerColor = Color.White
-        )
+                },
+                confirmButton = {
+                    Button(onClick = { dialogText = null }) {
+                        Text(text = stringResource(R.string.licenseDialog_confirm))
+                    }
+                },
+                containerColor = Color.White
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 private fun Preview() {
-    DependenciesScreen()
+    DependenciesScreen(EmptyDestinationsNavigator)
 }
