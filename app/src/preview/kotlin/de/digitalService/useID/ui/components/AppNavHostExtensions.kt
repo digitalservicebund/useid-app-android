d04b48d78092b294d119185f21fa2dcf152b3f4d
Ronny Brzeski <SvenBrzeski3@freenet.de>
2022-11-01 15:06:38 +0100

    Fix preview hilt bug

--- app/src/preview/kotlin/de/digitalService/useID/ui/components/AppNavHostExtensions.kt
+++ app/src/preview/kotlin/de/digitalService/useID/ui/components/AppNavHostExtensions.kt
@@ -5,10 +5,12 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import de.digitalService.useID.ui.screens.PreviewIdentificationFetchMetadata
 import de.digitalService.useID.ui.screens.PreviewIdentificationScan
 import de.digitalService.useID.ui.screens.PreviewSetupScan
+import de.digitalService.useID.ui.screens.identification.IdentificationScanViewModel
+import de.digitalService.useID.ui.screens.setup.SetupScanViewModel
 
 @Composable
 fun ConfigSpecificSetupScan() {
-    PreviewSetupScan(hiltViewModel(), hiltViewModel())
+    PreviewSetupScan(hiltViewModel(), hiltViewModel<SetupScanViewModel>())
 }
 
 @Composable
@@ -18,5 +20,5 @@ fun ConfigSpecificIdentificationFetchMetadata() {
 
 @Composable
 fun ConfigSpecificIdentificationScan() {
-    PreviewIdentificationScan(hiltViewModel(), hiltViewModel())
+    PreviewIdentificationScan(hiltViewModel(), hiltViewModel<IdentificationScanViewModel>())
 }
