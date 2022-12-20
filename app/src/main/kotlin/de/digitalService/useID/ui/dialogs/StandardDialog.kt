f6ae3c02528927094c8f4881d463ca7419b735f8
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2022-12-20 15:04:11 +0100

    Replace Dialog component with AlertDialog component

--- app/src/main/kotlin/de/digitalService/useID/ui/dialogs/StandardDialog.kt
+++ app/src/main/kotlin/de/digitalService/useID/ui/dialogs/StandardDialog.kt
@@ -1,14 +1,10 @@
 package de.digitalService.useID.ui.dialogs
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.window.Dialog
 import de.digitalService.useID.ui.theme.UseIdTheme
 
 @Composable
@@ -20,64 +16,59 @@ fun StandardDialog(
     onDismissButtonClick: (() -> Unit)? = null,
     onConfirmButtonClick: () -> Unit
 ) {
-    Dialog(onDismissRequest = onDismissButtonClick ?: { }) {
-        Column(
-            modifier = Modifier
-                .background(Color.White, shape = UseIdTheme.shapes.roundedMedium)
-                .padding(horizontal = UseIdTheme.spaces.s)
-                .padding(top = UseIdTheme.spaces.s)
-        ) {
-            title()
-
-            Spacer(modifier = Modifier.height(UseIdTheme.spaces.s))
-
-            text()
-
-            Spacer(modifier = Modifier.height(UseIdTheme.spaces.s))
-
-            Row(horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth()) {
-                if (dismissButtonText != null && onDismissButtonClick != null) {
-                    TextButton(onClick = onDismissButtonClick) {
-                        Text(
-                            text = dismissButtonText,
-                            style = UseIdTheme.typography.bodyLBold,
-                            color = UseIdTheme.colors.blue800
-                        )
-                    }
-                }
-
-                TextButton(onClick = onConfirmButtonClick) {
+    AlertDialog(
+        onDismissRequest = onDismissButtonClick ?: { },
+        dismissButton = {
+            if (dismissButtonText != null && onDismissButtonClick != null) {
+                TextButton(onClick = onDismissButtonClick) {
                     Text(
-                        text = confirmButtonText,
+                        text = dismissButtonText,
                         style = UseIdTheme.typography.bodyLBold,
                         color = UseIdTheme.colors.blue800
                     )
                 }
             }
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirmButtonClick) {
+                Text(
+                    text = confirmButtonText,
+                    style = UseIdTheme.typography.bodyLBold,
+                    color = UseIdTheme.colors.blue800
+                )
+            }
+        },
+        containerColor = UseIdTheme.colors.white,
+        shape = UseIdTheme.shapes.roundedMedium,
+        title = title,
+        text = text
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    UseIdTheme {
+        StandardDialog(
+            title = { Text(text = "Test Dialog") },
+            text = { Text(text = "test Dialog text") },
+            confirmButtonText = "Confirm"
+        ) {
         }
     }
 }
 
 @Preview
 @Composable
-private fun Preview() {
-    StandardDialog(
-        title = { Text(text = "Test Dialog") },
-        text = { Text(text = "test Dialog text") },
-        confirmButtonText = "Button text"
-    ) {
-    }
-}
-
-@Preview
-@Composable
 private fun PreviewWithDismissButton() {
-    StandardDialog(
-        title = { Text(text = "Test Dialog") },
-        text = { Text(text = "test Dialog text") },
-        confirmButtonText = "Button text",
-        dismissButtonText = "Dismiss",
-        onDismissButtonClick = {}
-    ) {
+    UseIdTheme {
+        StandardDialog(
+            title = { Text(text = "Test Dialog") },
+            text = { Text(text = "test Dialog text") },
+            confirmButtonText = "Confirm",
+            dismissButtonText = "Dismiss",
+            onDismissButtonClick = {}
+        ) {
+        }
     }
 }
