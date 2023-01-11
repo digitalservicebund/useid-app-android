0fef8fbb724b01104ce2b91f3f89eaa0b5320918
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2023-01-11 13:46:47 +0100

    Adapt SetupFinishViewModel tests

--- app/src/testDeviceRelease/kotlin/de/digitalService/useID/viewModel/SetupFinishViewModelTest.kt
+++ app/src/testDeviceRelease/kotlin/de/digitalService/useID/viewModel/SetupFinishViewModelTest.kt
@@ -2,9 +2,11 @@ package de.digitalService.useID.viewModel
 
 import de.digitalService.useID.ui.coordinators.SetupCoordinator
 import de.digitalService.useID.ui.screens.setup.SetupFinishViewModel
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -12,23 +14,25 @@ import org.junit.jupiter.api.extension.ExtendWith
 class SetupFinishViewModelTest {
 
     @MockK(relaxUnitFun = true)
-    lateinit var mockCoordinator: SetupCoordinator
+    lateinit var mockSetupCoordinator: SetupCoordinator
 
     @Test
-    fun onCloseButtonClicked() {
-        val viewModel = SetupFinishViewModel(mockCoordinator)
+    fun testIdentificationPending() {
+        val viewModel = SetupFinishViewModel(mockSetupCoordinator)
 
-//        viewModel.onCloseButtonClicked()
+        every { mockSetupCoordinator.identificationPending } returns false
+        Assertions.assertFalse(viewModel.identificationPending)
 
-//        verify(exactly = 1) { mockCoordinator.onBackToHome() }
+        every { mockSetupCoordinator.identificationPending } returns true
+        Assertions.assertTrue(viewModel.identificationPending)
     }
 
     @Test
-    fun onIdentifyButtonClicked() {
-        val viewModel = SetupFinishViewModel(mockCoordinator)
+    fun onOnButtonClicked() {
+        val viewModel = SetupFinishViewModel(mockSetupCoordinator)
 
         viewModel.onButtonClicked()
-
-        verify(exactly = 1) { mockCoordinator.finishSetup() }
+        
+        verify(exactly = 1) { mockSetupCoordinator.finishSetup() }
     }
 }
