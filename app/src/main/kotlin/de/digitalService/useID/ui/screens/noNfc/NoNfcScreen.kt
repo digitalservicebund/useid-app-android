c1a4a3801833014d01bb8bc6dd72a74528fd7548
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2023-03-01 15:13:23 +0100

    Remove unused import

--- app/src/main/kotlin/de/digitalService/useID/ui/screens/noNfc/NoNfcScreen.kt
+++ app/src/main/kotlin/de/digitalService/useID/ui/screens/noNfc/NoNfcScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
